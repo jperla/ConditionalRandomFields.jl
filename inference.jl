7fd48200f8f86a349655ae9e02b657aff72f7d78
Zack <zachary.chase@gmail.com>
2014-02-10 23:27:06 -0800

    inference modular

--- inference.jl
+++ inference.jl
@@ -57,40 +57,18 @@ function predict_label{T <: String}(weights::Array{Weight}, features::Features, 
     end
   end
 
-  #########################################################################
-  #   Retireve best score
-  ##########################################################################
-  best_score = 0
-  final_tag = 0
-  for j = 1:m
-    if s_lookup[n,j] > best_score
-     best_score = s_lookup[n,j]
-      final_tag = j
-    end
+  return best_label( last_tag, previous_tags)
+
+end
+
+function best_label(last_tag, previous_tags::Array{Float64, 2})
+
+  best_label = [last_tag]
+
+  prepend!(best_label, previous_tags[n, last_tag])
+  for n:-1:1
+
   end
-
-  #######################################################################
-  #  Retrieve best label
-  #######################################################################
-  # best_label = Int64[final_tag]
-  # for i = 2:n
-  #   prepend!(best_label, previous_tags[n-i+2,best_label[1]])
-  # end
-
-  println("$previous_tags")
-
-  #debug
-  println("n: $n, m: $m, final tag: $final_tag")
-
-
-  best_label = Tag[]
-  for i = 1:n-1
-    push!(best_label, SPACE)
-  end
-  push!(best_label, PERIOD)
-
-  return best_label
-
 end
 
 
