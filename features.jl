a871681c78cd0a2109403356eea2205634253d75
Joseph Perla <joseph@notanemail.com>
2014-02-12 02:47:34 -0500

    new more templates

--- features.jl
+++ features.jl
@@ -15,39 +15,49 @@ function is_word{T <: String}(word::T, i::Index, x::Vector{T})
   return booleanize(x[i] == word)
 end
 
-function is_tag{T <: String}(tag::Tag, i::Index, x::Vector{T}, yt::Tag, yt_before::Tag)
-  # Every possible tag at this position
-  return booleanize(yt == tag)
-end
-
 function is_n_to_last_word{T <: String}(j::Int, i::Index, x::Vector{T})
     return (length(x) - i) == j
 end
 
 function first_word_is{T <: String}(word::T, i::Index, x::Vector{T})
-    return (i == 1) && (lowercase(x[1]) == word)
+    return (i == 1 || i == 2) && (lowercase(x[1]) == word)
+end
 
 function word_ends_with{T <: String}(suffix::T, i::Index, x::Vector{T})
     return (endswith(x[i], suffix))
 end
 
+function is_tag{T <: String}(tag::Tag, i::Index, x::Vector{T}, yt::Tag, yt_before::Tag)
+  # Every possible tag at this position
+  return booleanize(yt == tag)
+end
+
+function tags_are{T <: String}(tags::(Tag,Tag), i::Index, x::Vector{T}, yt::Tag, yt_before::Tag)
+  # Every possible tag at this position
+  return booleanize(yt == tags[1] && yt_before == tags[2])
+end
+
+
 is_last_word_template = FeatureTemplate("%s to last word", is_n_to_last_word, [0, 1, 2])
 word_length_template = FeatureTemplate("word length is %s", word_length, [1, 2, 3, 4, 5, 6, 7, 8])
 dictionary_template = FeatureTemplate("word is \"%s\"", is_word, UTF8String["Graham", "Bill"])
 first_word_is_template = FeatureTemplate("first word is %s", first_word_is, UTF8String["what", "who", "when", "where", "how"])
-word_ends_with_template = FeatureTemplate("tag is %s", word_ends_with, UTF8String["ly", "ing"])
+word_ends_with_template = FeatureTemplate("word ends with %s", word_ends_with, UTF8String["ly", "ing"])
 
 one_tag_template = FeatureTemplate("tag is %s", is_tag, all_tags)
 
-two_tags = []
-for t1 in all_tags:
-    for t2 in all_tags:
+two_tags = Array((Tag,Tag), 0)
+for t1 in all_tags
+    for t2 in all_tags
         push!(two_tags, (t1, t2))
+    end
+end
+
 two_tag_template = FeatureTemplate("tags are %s", tags_are, two_tags)
 
 # All of our features in one convenient object
 our_a_templates = [dictionary_template, is_last_word_template, word_ends_with_template, first_word_is_template]
-our_b_templates = [one_tag_template, two_tag_template]
+our_b_templates = [one_tag_template, ]
 our_templatized_features = TemplatizedFeatures(our_a_templates, our_b_templates)
 our_features = build_features(our_templatized_features)
 
