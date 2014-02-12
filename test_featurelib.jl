187fcc1e7e7afe2d4ea5b9d8c6996cd5fbb6549e
Zack <zachary.chase@gmail.com>
2014-02-11 23:14:01 -0500

    starting test_featurelib

--- test_featurelib.jl
+++ test_featurelib.jl
@@ -1,7 +1,13 @@
 using Base.Test
-include("featurelib.jl")
+include("features.jl")
+
+###############################################################
+#   Test evaluate_feature
+###############################################################
+
+
+test_features = build_features(TemplatizedFeatures([dictionary_template], [one_tag_template]))
 
 #
+#  Bill = Start
 #
-#
-
