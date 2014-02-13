9f99a00e917e87b4a4de941b551c3213dbcf9c14
Joseph Perla <joseph@notanemail.com>
2014-02-13 16:09:49 -0500

    run contrastive divergence script

---
+++ run_contrastive_divergence.jl
@@ -0,0 +1,14 @@
+using Base.Test
+require("contrastive_divergence.jl")
+require("features.jl")
+require("read_data.jl")
+
+crf = ContrastiveDivergenceCRF(all_tags, our_features, 0.1, 3)
+println("num features: ", num_features(crf))
+
+fit!(crf, train_sentences[1:300], train_labels[1:300], test_data=test_sentences[1:100], test_labels=test_labels[1:100])
+
+top_features(crf.features, crf.w_, n=20)
+
+@test predict(crf, UTF8String["Bill", "Graham", "is", "dead"]) == [SPACE, SPACE, SPACE, PERIOD]
+
